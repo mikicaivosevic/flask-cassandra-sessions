66bf487d6031947291fd1aee532994900140466d
Mikica Ivosevic <mikica.ivosevic@gmail.com>
2016-11-06 11:07:47 +0100

    update version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'cassandra_flask_sessions',
   packages = ['cassandra_flask_sessions'], # this must be the same as the name above
-  version = '0.2',
+  version = '0.3',
   description = 'Server side sessions with Apache Cassandra',
   author = 'Mikica Ivosevic',
   author_email = 'mikica.ivosevic@gmail.com',
   url = 'https://github.com/mikicaivosevic/flask-cassandra-sessions', # use the URL to the github repo
-  download_url = 'https://github.com/mikicaivosevic/flask-cassandra-sessions/archive/0.2.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/mikicaivosevic/flask-cassandra-sessions/archive/0.3.tar.gz', # I'll explain this in a second
   keywords = ['flask', 'cassandra', 'sessions'],
   classifiers = [],
 )
